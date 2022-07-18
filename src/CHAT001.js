5b6398504837627637c5f406607da22f19d8c76b
dogfoot_log <dogfoot_log@naver.com>
2022-07-18 23:09:19 +0900

    [220718] template 만들기

---
+++ src/CHAT001.js
@@ -0,0 +1,32 @@
+const mockData = [
+  {
+    id: 1,
+    categoryName: 'friend',
+    list: [
+      { subId: 1, name: '아이유' },
+      { subId: 2, name: '사쿠라' },
+    ],
+  },
+  {
+    id: 2,
+    categoryName: 'friend',
+    list: [
+      { subId: 1, name: '아이유' },
+      { subId: 2, name: '사쿠라' },
+    ],
+  },
+  {
+    id: 3,
+    categoryName: 'friend',
+    list: [
+      { subId: 1, name: '아이유' },
+      { subId: 2, name: '사쿠라' },
+    ],
+  },
+];
+
+const CHAT001 = () => {
+  return <div></div>;
+};
+
+export default CHAT001;
