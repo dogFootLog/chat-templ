cb30b46accb97d367968aa26e28e2bc8cc58937a
dogfoot <dogfoot_log@naver.com>
2022-07-19 01:54:54 +0900

    [220719] chat-platform

--- src/CHAT001.js
+++ src/CHAT001.js
@@ -1,32 +1,30 @@
-const mockData = [
-  {
-    id: 1,
-    categoryName: 'friend',
-    list: [
-      { subId: 1, name: '아이유' },
-      { subId: 2, name: '사쿠라' },
-    ],
-  },
-  {
-    id: 2,
-    categoryName: 'friend',
-    list: [
-      { subId: 1, name: '아이유' },
-      { subId: 2, name: '사쿠라' },
-    ],
-  },
-  {
-    id: 3,
-    categoryName: 'friend',
-    list: [
-      { subId: 1, name: '아이유' },
-      { subId: 2, name: '사쿠라' },
-    ],
-  },
-];
-
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import fetcher from './fetcher';
 const CHAT001 = () => {
-  return <div></div>;
+  const [friends, setFriends] = useState([]);
+
+  const navigate = useNavigate();
+
+  const goTo002 = async (id) => {
+    const res = await fetcher('get', `/sakura/${id}`);
+    console.log(typeof res, res);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    navigate('/CHAT002', { state: res });
+  };
+
+  useEffect(() => {
+    const getTotalApi = async () => {
+      const res = await fetcher('get', '/chat');
+      console.log(res);
+    };
+    getTotalApi();
+  }, []);
+  return (
+    <div>
+      <button onClick={() => goTo002(2)}>click</button>
+    </div>
+  );
 };
 
 export default CHAT001;
--- src/CHAT002.js
+++ src/CHAT002.js
@@ -1,5 +1,20 @@
+import { useLocation } from 'react-router-dom';
+
+import styles from './CHAT002.module.css';
+
 const CHAT002 = () => {
-  return <div></div>;
+  const location = useLocation();
+  console.log('chat2 location: ', location.state);
+  return (
+    <div>
+      <div className={styles['input-wrap']}>
+        <input id="message" type="text" value="" onChange={() => {}} />
+        <button type="button" onClick={() => {}}>
+          enter
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CHAT002;
